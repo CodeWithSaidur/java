0ed5dc6c1b0ee7f587b0b58c5fce0306a16d1105
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-02-12 23:24:01 +0530

    01

---
+++ Code/java_2.java
@@ -0,0 +1,14 @@
+public class java_2 {
+	public static void main(String[] args){
+
+		String name = "Code With Saidur !";
+		int age = 25;
+		double height = 5.5;
+		boolean isAiUser = true;
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(isAiUser);
+		System.out.println(height);
+	}
+}
